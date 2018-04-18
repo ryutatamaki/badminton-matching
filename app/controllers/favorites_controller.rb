1ff3921d3ddb69b92418ae892d4c592c7b48e4a9
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2018-04-18 09:44:42 +0000

    ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
-    flash[:success] =  "いいねしました"
+    # flash[:success] =  "いいねしました"
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
-    flash[:danger] = "いいねを解除しました"
+    # flash[:danger] = "いいねを解除しました"
     redirect_back(fallback_location: root_path)
   end
 end
